0183e34e6588c28b3116954c23e114d2bc37703a
Carina <c.malinowska@freenet.de>
2017-12-05 14:56:00 +0100

    Notizen Anwendung hinzugefügt und mit HaushaltActivity verknüpft

--- app/src/main/java/com/example/carina/haushaltsapp/HaushaltActivity.java
+++ app/src/main/java/com/example/carina/haushaltsapp/HaushaltActivity.java
@@ -1,13 +1,82 @@
 package com.example.carina.haushaltsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.carina.haushaltsapp.R.id.*;
 
 public class HaushaltActivity extends AppCompatActivity {
+private Button btnEinkauf, btnAusgaben, btnToDo, btnNotizen, btnFun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haushalt);
+        InitializeApp();
     }
+
+    private void InitializeApp() {
+        btnEinkauf = (Button) findViewById(button1);
+        btnAusgaben = (Button) findViewById(button2);
+        btnToDo = (Button) findViewById(button3);
+        btnNotizen = (Button) findViewById(button4);
+
+        /*btnEinkauf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startEinkauf();
+            }
+        });
+
+        btnAusgaben.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAusgaben();
+            }
+        });
+
+        btnToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startToDoList();
+            }
+        });
+*/
+        btnNotizen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNotizen();
+            }
+        });
+    }
+
+    /** Start the Other Activities
+
+     private void startEinkauf() {
+        startActivity(
+         new Intent(this, Einkauf.class));
+     }
+
+
+     private void startAusgaben() {
+         startActivity(
+         new Intent(this, Ausgaben.class));
+     }
+
+
+     private void startToDoList() {
+         startActivity(
+         new Intent(this, ToDoList.class));
+     }
+     */
+
+
+    private void startNotizen() {startActivity(
+                new Intent(this, MyNotes.class));
+    }
+
+
 }
